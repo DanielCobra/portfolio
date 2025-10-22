317f7ad0c20cf05741aec14d9b9f515d7e76b1f7
DanielCobra <82939086+DanielCobra@users.noreply.github.com>
2025-10-22 18:01:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+const mySwiper = new Swiper('.swiper-container', {
+    pagination : {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    },
+  });
